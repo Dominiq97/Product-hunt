c775f880efa9570f0882c62c7f240d2893b24e2c
Domicles <37049614+Dominiq97@users.noreply.github.com>
2021-09-08 14:59:19 +0300

    Update settings.py

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*hhmim---9wdz_ca1(&#!!$q#0@o2f4cm=d^730o@y@!387jlf'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
